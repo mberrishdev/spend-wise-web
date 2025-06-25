67050f1e5e53fadd98f05129132f09c06f666158
Mikheil Berishvili <mikheil.berishvili12@gmail.com>
2025-06-25 10:57:38 +0400

    add cache clear

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,21 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import './i18n'
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import "./i18n";
 
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.getRegistrations().then((registrations) => {
-      for (const reg of registrations) {
-        reg.unregister();
-      }
-    });
-  }
-  
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    for (const reg of registrations) {
+      reg.unregister();
+    }
+  });
+
+  // 💥 Clear service worker caches
+  caches.keys().then((names) => {
+    for (const name of names) {
+      caches.delete(name);
+    }
+  });
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
